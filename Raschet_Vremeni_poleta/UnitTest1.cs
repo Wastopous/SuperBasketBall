ca864c7c7fea8243f1ad2da189e1f29424142bc7
Wastopous <wastopous23@mail.ru>
2024-04-15 06:42:47 +0700

    3

---
+++ Raschet_Vremeni_poleta/UnitTest1.cs
@@ -0,0 +1,41 @@
+namespace Raschet_Vremeni_poleta;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+
+        
+    }
+    private double CalcT(double v, double g)
+    {
+        return  2 * v / g;
+    }
+
+    [Test]
+    public void TestWhenVisHalfofG()
+    {
+        double v = 4.9;
+        double g = 9.8;
+        double t = 2 * v / g;
+
+        double calcT = CalcT(v, g);
+        
+        Assert.AreEqual(t, calcT, 1);
+
+    }
+    
+    [Test]
+    public void TestWhenVisNull()
+    {
+        double v = 0;
+        double g = 9.8;
+        double t = 2 * v / g;
+
+        double calcT = CalcT(v, g);
+        
+        Assert.AreEqual(t, calcT, 1);
+
+    }
+}
